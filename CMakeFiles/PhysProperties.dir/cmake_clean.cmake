5c17567f331298efa8d7eb8e1caf7a1a0eecdcaa
oster <oleg.ternikov@gmail.com>
2024-03-26 23:57:18 +0300

    CMakeLists decomposed

---
+++ CMakeFiles/PhysProperties.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PhysProperties.dir/antlr4cpp_generated_src/TempBound/TempBoundBaseListener.cpp.o"
+  "CMakeFiles/PhysProperties.dir/antlr4cpp_generated_src/TempBound/TempBoundBaseListener.cpp.o.d"
+  "CMakeFiles/PhysProperties.dir/antlr4cpp_generated_src/TempBound/TempBoundLexer.cpp.o"
+  "CMakeFiles/PhysProperties.dir/antlr4cpp_generated_src/TempBound/TempBoundLexer.cpp.o.d"
+  "CMakeFiles/PhysProperties.dir/antlr4cpp_generated_src/TempBound/TempBoundListener.cpp.o"
+  "CMakeFiles/PhysProperties.dir/antlr4cpp_generated_src/TempBound/TempBoundListener.cpp.o.d"
+  "CMakeFiles/PhysProperties.dir/antlr4cpp_generated_src/TempBound/TempBoundParser.cpp.o"
+  "CMakeFiles/PhysProperties.dir/antlr4cpp_generated_src/TempBound/TempBoundParser.cpp.o.d"
+  "antlr4cpp_generated_src/TempBound/TempBoundBaseListener.cpp"
+  "antlr4cpp_generated_src/TempBound/TempBoundBaseListener.h"
+  "antlr4cpp_generated_src/TempBound/TempBoundLexer.cpp"
+  "antlr4cpp_generated_src/TempBound/TempBoundLexer.h"
+  "antlr4cpp_generated_src/TempBound/TempBoundLexer.interp"
+  "antlr4cpp_generated_src/TempBound/TempBoundLexer.tokens"
+  "antlr4cpp_generated_src/TempBound/TempBoundListener.cpp"
+  "antlr4cpp_generated_src/TempBound/TempBoundListener.h"
+  "antlr4cpp_generated_src/TempBound/TempBoundParser.cpp"
+  "antlr4cpp_generated_src/TempBound/TempBoundParser.h"
+  "libPhysProperties.pdb"
+  "libPhysProperties.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PhysProperties.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ antlr4_runtime/runtime/antlr4_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/oster/Electrolisys/antlr4_runtime/runtime/antlr4_tests[1]_tests.cmake")
+  include("/home/oster/Electrolisys/antlr4_runtime/runtime/antlr4_tests[1]_tests.cmake")
+else()
+  add_test(antlr4_tests_NOT_BUILT antlr4_tests_NOT_BUILT)
+endif()
