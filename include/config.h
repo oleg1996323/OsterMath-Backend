134293623f3e4cb8bdae96c8a4e0172de1f8e3a6
oster <oleg.ternikov@gmail.com>
2024-04-04 17:57:29 +0300

    Boost parser in process

--- include/config.h
+++ include/config.h
@@ -1,5 +1,7 @@
 #include <boost/json.hpp>
 #include <boost/json/src.hpp>
+#include <string_view>
+#include <boost/static_string/static_string.hpp>
 
 #ifdef unix
     static const std::string ROOT_DIR = std::string(__FILE__).substr(0, std::string(__FILE__).find_last_of("/"));
@@ -12,6 +14,7 @@ using namespace boost::json;
 class JsonConfigInit{
     public:
     JsonConfigInit(){
-        boost::json::key_value_pair;
+        boost::json::object paths;
+        paths.emplace();
     }
 };
